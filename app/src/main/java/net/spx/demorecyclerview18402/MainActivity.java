d80c0653e2aa8b1bdbcb0def925bf1074cd388ff
snvn1511 <spxedu@gmail.com>
2023-11-09 11:06:08 +0700

    Tạo đối tượng lấy dữ liệu và gắn adapter cho recyclerview trong activity

--- app/src/main/java/net/spx/demorecyclerview18402/MainActivity.java
+++ app/src/main/java/net/spx/demorecyclerview18402/MainActivity.java
@@ -1,14 +1,35 @@
 package net.spx.demorecyclerview18402;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import net.spx.demorecyclerview18402.Adapter.CatAdapter;
+import net.spx.demorecyclerview18402.DAO.CatDAO;
+import net.spx.demorecyclerview18402.DTO.CatDTO;
 
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    CatDAO catDAO;
+    ArrayList<CatDTO> listCat;
+    RecyclerView rc_cat;
+    CatAdapter catAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rc_cat = findViewById(R.id.rc_cat);
+
+        catDAO = new CatDAO(this);
+
+        listCat = catDAO.getList();
+
+        catAdapter = new CatAdapter(listCat, this);
+
+        rc_cat.setAdapter( catAdapter );
+
     }
 }
